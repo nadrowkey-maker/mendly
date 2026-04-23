efc08aac6f4ad24b5043969acaf8ae17cfd392ea
Flavien Gaude <flavien.kmoov@gmail.com>
2026-04-23 11:13:03 +0200

    fix: adapt config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  ...(isProd && { output: 'export', basePath: '/mendly' }),
   images: {
-    unoptimized: true,
+    unoptimized: false,
   },
   trailingSlash: true,
 };
