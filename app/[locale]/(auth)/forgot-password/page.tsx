9c8ae0d3afe73b3b9954e8cfd4d9e05a22400824
Flavien Gaude <flavien.kmoov@gmail.com>
2026-05-04 13:48:37 +0200

    update

--- app/[locale]/(auth)/forgot-password/page.tsx
+++ app/[locale]/(auth)/forgot-password/page.tsx
@@ -22,7 +22,7 @@ export default function ForgotPasswordPage() {
     setErrorMsg("");
 
     const { error } = await supabase.auth.resetPasswordForEmail(email, {
-      redirectTo: `${window.location.origin}/auth/callback?next=/${locale}/reset-password`,
+      redirectTo: `${window.location.origin}/${locale}/reset-password`,
     });
 
     if (error) {
